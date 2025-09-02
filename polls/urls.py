145711b847593ee2d9e0fbfd931d49ff8a70836d
Brayan <baguevaram@unal.edu.co>
2025-09-02 15:27:50 +0200

    Implement polls app views and templates for question detail and index

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("p1", views.p1, name="p1"),
+    path('owner', views.owner, name='owner'),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
